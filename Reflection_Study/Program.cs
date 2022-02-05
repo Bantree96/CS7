79ee9467ffcfee48b960f5520bb4c665c5744ea7
남지원 <51081373+Bantree96@users.noreply.github.com>
2022-02-05 11:20:29 +0900

    공부한거 올림

---
+++ Reflection_Study/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reflection;
+using PropertyChanged;
+
+namespace Reflection_Study
+{
+    // [AddINotifyPropertyChangedInterface]
+    class Program
+    {
+        // AppDomain은 콘솔프로그램에서만 나온다.
+        static AppDomain currentDomain = AppDomain.CurrentDomain;
+
+        static void Main(string[] args)
+        {
+            // Module_Type();
+            Assembly_Type(currentDomain);
+        }
+
+        /// <summary>
+        /// 모듈의 타입을 출력한다.
+        /// </summary>
+        static void Module_Type(AppDomain currentDomain)
+        {
+            
+            Console.WriteLine("Current Domain Name: " + currentDomain.FriendlyName);
+            foreach (Assembly asm in currentDomain.GetAssemblies())
+            {
+                // 파일 이름과 버전 등이 나옴
+                Console.WriteLine("  " + asm.FullName);
+
+                foreach (Module module in asm.GetModules())
+                {
+                    // DLL, EXE 경로가 나옴
+                    Console.WriteLine("  " + module.FullyQualifiedName);
+
+                    foreach (Type type in module.GetTypes())
+                    {
+                        // 그 DLL or EXE에서 사용할수있는 타입 -> using으로 사용중인 모든것
+                        Console.WriteLine("  " + type.FullName);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 어셈블리의 타입을 출력한다.
+        /// </summary>
+        static void Assembly_Type(AppDomain currentDomain)
+        {
+            foreach (Assembly asm in currentDomain.GetAssemblies())
+            {
+                Console.WriteLine("  " + asm.FullName);
+
+                foreach (Type type in asm.GetTypes())
+                {
+                    Console.WriteLine("  " + type.FullName);
+
+                }
+            }
+        }
+    }
+}
---
+++ ThreadPool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPool_Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(Calc);
+            ThreadPool.QueueUserWorkItem(Calc, 10.0);
+            ThreadPool.QueueUserWorkItem(Calc, 20.0);
+        }
+
+        static void Calc(object radius)
+        {
+            if (radius == null) return;
+
+            double r = (double)radius;
+            double area = r * r * 3.14;
+            Console.WriteLine($"r={r}, area={area}");
+        }
+    }
+}
