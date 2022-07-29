1d5b1bbe1cae1abc5082ab0f8fc7758d8476e973
남지원 <jwnam@crevis.co.kr>
2022-07-29 10:34:29 +0900

    소수점 버리기 코드

---
+++ Double_Round/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Round
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numA = 123.456789;
+            double numB = Math.Truncate(numA * 100) / 100;
+            Console.WriteLine(numB);
+        }
+    }
+}
