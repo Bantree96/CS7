9a6df11e3ac47cee4d0da8058b9c01e5cfa9feac
남지원 <jwnam@crevis.co.kr>
2022-11-15 10:55:40 +0900

    string Trim해서 ,빼는 작업

---
+++ String_Trim/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Trim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "[1,0.99],[2,1],";
+            str1.TrimEnd(',');
+        }
+    }
+}
