21ce29874229d342c9e5928347864a19ca9fde18
찌원 남 <skawldnjs125@naver.com>
2022-11-26 12:52:54 +0900

    얕은복사 프로젝트

---
+++ ShallowCopy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShallowCopy
+{
+    public class Camera
+    {
+        public int No;
+        public string Name;
+
+        public Camera ShallowCopy()
+        {
+            return this.MemberwiseClone() as Camera;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Camera cam = new Camera();
+            cam.No = 0;
+            cam.Name = "Samsung";
+
+            Camera cam2 = cam.ShallowCopy();
+            cam2.No = 1;
+            cam2.Name = "Sony";
+
+            Console.WriteLine($"{cam.No} : {cam.Name}");
+            Console.WriteLine($"{cam2.No} : {cam2.Name}");
+
+        }
+    }
+}
