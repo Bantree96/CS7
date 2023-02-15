929f7d7340520f3c7d511f89aa0357c46517c49e
남지원 <jwnam@crevis.co.kr>
2023-02-15 13:29:59 +0900

    Eterator를 사용할때 Finally 처리가 어떻게 되는지

---
+++ FinallyEterator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinallyEterator
+{
+    class Program
+    {
+        static IEnumerable<string> Iterator()
+        {
+            try
+            {
+                Console.WriteLine("Before first yield");
+                yield return "first";
+                Console.WriteLine("Between yield");
+                yield return "Second";
+                Console.WriteLine("After second yield");
+            }
+            finally
+            {
+                Console.WriteLine("In Finally Block");
+            }
+        }
+        static void Main(string[] args)
+        {
+            foreach(string value in Iterator())
+            {
+                Console.WriteLine($"Received value : {value}");
+            }
+        }
+    }
+}
