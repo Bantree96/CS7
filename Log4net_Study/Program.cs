96c8ea16f5e013c71ee30134ed4659b2a6fbd765
남지원 <jwnam@crevis.co.kr>
2023-06-01 12:18:00 +0900

    Log4net

---
+++ Log4net_Study/Program.cs
@@ -0,0 +1,18 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4net_Study
+{
+    class Program
+    {
+            private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            _log.Info("hello");
+        }
+    }
+}
