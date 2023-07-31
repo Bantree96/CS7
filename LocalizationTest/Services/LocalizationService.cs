cf47bba7874e1099cb051c50230d44c1e8790bdf
남지원 <jwnam@crevis.co.kr>
2023-07-31 13:54:25 +0900

    localization 만들기

---
+++ LocalizationTest/Services/LocalizationService.cs
@@ -0,0 +1,87 @@
+﻿using LocalizationTest.Resources;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace LocalizationTest.Services
+{
+    public class LocalizationService : DynamicObject
+    {
+        public event EventHandler<string> LanguageChanged;
+
+        private readonly ResourceManager _resourceManager;
+        private CultureInfo _cultureInfo;
+
+        public LocalizationService()
+        {
+            _resourceManager = new ResourceManager(typeof(Resource));
+
+        }
+
+        /// <summary>
+        /// Property로 호출
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public string this[string id]
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(id)) return null;
+                string str = _resourceManager.GetString(id, _cultureInfo);
+                if (string.IsNullOrEmpty(str))
+                {
+                    str = id;
+                }
+                return str;
+            }
+        }
+
+        /// <summary>
+        /// 이름으로 호출
+        /// </summary>
+        /// <param name="binder"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            string id = binder.Name;
+            string str = _resourceManager.GetString(id, _cultureInfo);
+            if (string.IsNullOrEmpty(str))
+            {
+                str = id;
+            }
+            result = str;
+            return true;
+        }
+
+        public void ChangeLanguage(string languageCode)
+        {
+            _cultureInfo = new CultureInfo(languageCode);
+            Thread.CurrentThread.CurrentCulture = _cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = _cultureInfo;
+
+            // 윈도우의 언어코드 변경
+            foreach(Window window in Application.Current.Windows.Cast<Window>())
+            {
+                if (!window.AllowsTransparency)
+                {
+                    window.Language = XmlLanguage.GetLanguage(_cultureInfo.Name);
+                }
+            }
+
+            if(LanguageChanged != null)
+            {
+                LanguageChanged.Invoke(this, _cultureInfo.Name);
+            }
+        }
+    }
+}
---
+++ LocalizationTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using LocalizationTest.Services;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalizationTest.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private LocalizationService _localizationService;
+
+        private string _title;
+        public string Title { get => _title; set => SetProperty(ref _title, value); }
+
+        public DelegateCommand<string> LanguageChangeCommand => new DelegateCommand<string>(OnLanguageChanged);
+        
+
+        public MainWindowViewModel(LocalizationService localizationService)
+        {
+            _localizationService = localizationService;
+
+            //_localizationService = App.Current.Resources["DR"] as LocalizationService;
+            _localizationService.LanguageChanged += LanguageChanged;
+
+            // WPF 언어는 기본적으로 en-US인 상태이기 때문에 언어변경을 한번 해줘야 영어로 사용 가능.
+            _localizationService.ChangeLanguage("ko-KR");
+            _localizationService.ChangeLanguage("en-US");
+        }
+
+        private void OnLanguageChanged(string obj)
+        {
+            _localizationService.ChangeLanguage(obj);
+        }
+
+        // 언어 변경됐을 때 ViewModel에서 처리할 내용
+        private void LanguageChanged(object sender, string e)
+        {
+            Title = _localizationService["Title"];
+        }
+    }
+}
