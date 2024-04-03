f6f505205327a5e18f13929a7722a96033ac27e6
남지원 <jwnam@crevis.co.kr>
2024-04-03 12:26:22 +0900

    Bitmap저장깨짐 현상 테스트

---
+++ Bitmap_Save_Crash/BitmapManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitmap_Save_Crash
+{
+	class BitmapManager
+	{
+	}
+}
